d9b85960e7ff9236226223df60d8a7b8fbe0eaa4
susmithametta9-debug <susmithametta9@gmail.com>
2026-04-01 15:40:54 -0400

    refractor done

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -9,6 +9,10 @@ import android.widget.ImageView
 import android.widget.TextView
 import java.util.Locale
 
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.delay
+
 class MainActivity : AppCompatActivity() {
 
     //TODO (Refactor to replace Thread code with coroutines)
@@ -21,24 +25,35 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.currentTextView)
     }
 
-    val handler = Handler(Looper.getMainLooper(), Handler.Callback {
-
-        currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it.what)
-        cakeImageView.alpha = it.what / 100f
-        true
-    })
+//    val handler = Handler(Looper.getMainLooper(), Handler.Callback {
+//
+//        currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it.what)
+//        cakeImageView.alpha = it.what / 100f
+//        true
+//    })
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
+//        findViewById<Button>(R.id.revealButton).setOnClickListener{
+//            Thread{
+//                repeat(100) {
+//                    handler.sendEmptyMessage(it)
+//                    Thread.sleep(40)
+//                }
+//            }.start()
+//        }
+
+        findViewById<Button>(R.id.revealButton).setOnClickListener {
+            lifecycleScope.launch {
                 repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
+                    currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it)
+
+                    cakeImageView.alpha = it/100f
+                    delay(40)
                 }
-            }.start()
+            }
         }
     }
 }
